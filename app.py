51f7dd35809813f2f6b13ccf5bcaa4d85056e2df
sahil7010z <sahilgupta7010@gmail.com>
2025-06-19 20:06:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 from routes.dashboard import dashboard_bp
 from routes.api_routes import api_bp
+import os
 
 app = Flask(__name__)
 
-# Register routes
+# Register Blueprints
 app.register_blueprint(dashboard_bp)
 app.register_blueprint(api_bp)
 
@@ -13,4 +14,5 @@ def home():
     return render_template('try_now.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # for Render dynamic port
+    app.run(host='0.0.0.0', port=port)
